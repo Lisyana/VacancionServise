b12c456ea144265ecacf22eab20e47c1bf24a9ee
Lisyana <89046331518@mail.ru>
2024-06-12 19:15:27 +0300

    test optimization

--- src/main/java/ru/netology/javaqa/Vacantion/service/VacancionServise.java
+++ src/main/java/ru/netology/javaqa/Vacantion/service/VacancionServise.java
@@ -2,20 +2,21 @@ package ru.netology.javaqa.Vacantion.service;
 
 public class VacancionServise {
 
-    public int calcVacancionMonth(int income, int expenses, int threshold){
+    public int calcVacationMonths(int income, int expenses, int threshold) {
         int money = 0;
-        int vacancionMonth = 0;
-        for (int i = 0; i < 12; i ++){
-            if (money < threshold){
-                money = money + income;
-                money = money - expenses;
+        int vacationMonths = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money -= expenses;
+                money /= 3;
+                vacationMonths++;
             } else {
-                money = money - expenses;
-                money = money / 3;
-                vacancionMonth ++;
+                money += income;
+                money -= expenses;
             }
         }
-        return vacancionMonth;
 
+        return vacationMonths;
     }
 }
--- src/test/java/VacationServiceTest.java
+++ src/test/java/VacationServiceTest.java
@@ -1,29 +1,16 @@
 package ru.netology.javaqa.Vacantion.service;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
-    @Test
-    public void testCalcVacationMonth() {
-        VacancionServise servise = new VacancionServise();
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-        int expected = 3;
 
-        int actual = servise.calcVacancionMonth(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void testCalcVacationMonthsExample2() {
-        VacancionServise servise = new VacancionServise();
-        int income = 100000;
-        int expenses = 60000;
-        int threshold = 150000;
-        int expected = 2;
-
-        int actual = servise.calcVacancionMonth(income, expenses, threshold);
+    @ParameterizedTest
+    @CsvFileSource(files="src/main/resources/vacancion.csv")
+    public void testCalcVacationMonths(int income, int expenses, int threshold, int expected) {
+        VacancionServise service = new VacancionServise();
+        int actual = service.calcVacationMonths(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
